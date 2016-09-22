b4714e4fff1b9852e211a5ca5a8b79de632d71ad
Ilya <ilyau@users.noreply.github.com>
2016-09-22 23:27:55 +0300

    refactoring

--- background.js
+++ background.js
@@ -1,15 +1,6 @@
-function runAction() {
-    chrome.tabs.executeScript({
-        file: "action.js"
-    });
-}
-
 chrome.browserAction.onClicked.addListener(function (tab) {
-    runAction();
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	  chrome.tabs.sendMessage(tabs[0].id, {event: "refreshcss"});
+	});
 });
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.greeting == "refreshcss") {
-        runAction();
-    }
-});
--- content.js
+++ content.js
@@ -2,6 +2,23 @@ window.onkeyup = function(e) {
    var key = e.keyCode ? e.keyCode : e.which;
 
     if (key == 120) {
-       	chrome.runtime.sendMessage({greeting: "refreshcss"});
+       	refreshCSS();
    	}
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	if (request.event == "refreshcss") {
+		refreshCSS();
+	}
+});
+
+function refreshCSS() {
+	var queryString = '?reload=' + new Date().getTime();
+	var links = document.getElementsByTagName("link"); 
+	for (var i = 0; i < links.length;i++) { 
+	    var link = links[i]; 
+	    if (link.rel === "stylesheet") {
+	        link.href = link.href.replace(/\?.*|$/, queryString);
+	    }
+	}
 }
